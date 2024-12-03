f8e9a81d49bbd080edb84544a221bbeb0cdab395
TABLET-6FSNT7EB\gojin <larc.ituki@gmail.com>
2024-12-04 08:20:10 +0900

    prismaのセットアップ

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-export default function Home() {
+import { neon } from '@neondatabase/serverless';
+
+export default function Page() {
+  async function create(formData: FormData) {
+    'use server';
+    // Connect to the Neon database
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const comment = formData.get('comment');
+    // Insert the comment from the form into the Postgres database
+    await sql('INSERT INTO comments (comment) VALUES ($1)', [comment]);
+  }
+
   return (
-    <main>
-      <p>テスト</p>
-    </main>
+    <form action={create}>
+      <input type="text" placeholder="write a comment" name="comment" />
+      <button type="submit">Submit</button>
+    </form>
   );
 }
